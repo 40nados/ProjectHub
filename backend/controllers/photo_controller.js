5c4efed9d62590e486383b7d23cf564b7029d558
Wesley Santos <wesleysantos32892653@outlook.com>
2024-08-08 20:25:39 -0300

    Controller PhotoById Feito

--- backend/controllers/photo_controller.js
+++ backend/controllers/photo_controller.js
@@ -1,6 +1,7 @@
 //InsertPhoto
 //Delete_photo
-//GettAllPhotos
+
+//GetAllPhotos
 //GetphotobyId
 //GetuserPhotos
 
@@ -27,7 +28,7 @@ const InsertPhoto = async (req, res) => {
   });
 
   if (!newPhoto) {
-    res.status(422).json({ errors: "Error. Try another photo." });
+    res.status(422).json({ errors: "Error. Try later." });
 
     return;
   }
@@ -44,23 +45,54 @@ const DeletePhoto = async (req, res) => {
   try {
     const photo = await Photo.findById(id);
 
+    //Photo dosn't exist - Foto Não existe
     if (!photo) {
       res.status(404).json({ errors: "Photo not found" });
       return;
     }
+
+    //Photo belongs to user - Foto pertence ao usuário
     if (!photo.userId.equals(reqUser._id)) {
       res.status(403).json({ errors: "You can't delete this photo" });
     }
 
+    //Deleting photo by DB - Deletando foto do banco
     await Photo.findByIdAndDelete(photo._id);
 
     res
       .status(200)
-      .json({ id: photo.id, message: "Foto apagada com sucesso!" });
+      .json({ id: photo.id, message: "Photo deleted succesfully!" }); // Success Message - Mensagem de Sucesso
   } catch (error) {
-    res.status(500).json({ errors: "Erro interno no servidor" });
+    res.status(500).json({ errors: "Intern Server Error" });
     return;
   }
 };
 
-module.exports = { InsertPhoto, DeletePhoto }; //Exportando las funciones
+//GetAllPhotos
+const GetAllPhotos = async (req, res) => {};
+
+//GetPhotoById
+const PhotoById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const photo = Photo.findById(id);
+
+    if (!photo) {
+      res.status(404).json({ errors: "Photo not found" }); // Foto não encontrada
+      return;
+    }
+
+    res.status(200).json(photo);
+  } catch (error) {
+    res.status(500).json({ errors: "Intern Server Error" }); // Algum erro aí
+    return;
+  }
+};
+
+module.exports = {
+  InsertPhoto,
+  DeletePhoto,
+  GetAllPhotos,
+  PhotoById,
+}; //Exportando las funciones
