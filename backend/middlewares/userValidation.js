4fe94c7e16e98198929a6a482ca30fc168349910
Wesley Santos <wesleysantos32892653@outlook.com>
2024-08-29 11:22:03 -0300

    Atualizando mensagem de erro de validação de usuário para idioma padrão do projeto (inglês)

--- backend/middlewares/userValidation.js
+++ backend/middlewares/userValidation.js
@@ -1,72 +1,69 @@
-const { body } = require("express-validator");
+const { body } = require('express-validator');
 
 const createUserValidation = () => {
-  return [
-    body("username")
-      .isString()
-      .withMessage("O Username é obrigatório!")
-      .isLength({ min: 3 })
-      .withMessage("O Username precisa ter no mínimo 3 caracteres."),
-    body("email")
-      .isString()
-      .withMessage("O Email é obrigatório!")
-      .isEmail()
-      .withMessage("O Email precisa ser válido!"),
-    body("password")
-      .isString()
-      .withMessage("A senha é obrigatória!")
-      .isLength({ min: 5 })
-      .withMessage("A senha deve ter ao menos 5 caracteres."),
-    body("language").notEmpty().withMessage("A linguagem é obrigatória!"),
-  ];
+    return [
+        body('username')
+            .isString()
+            .withMessage('Username is required!')
+            .isLength({ min: 3 })
+            .withMessage('Username must be at least 3 characters long.'),
+        body('email')
+            .isString()
+            .withMessage('Email is required!')
+            .isEmail()
+            .withMessage('Email must be valid!'),
+        body('password')
+            .isString()
+            .withMessage('Password is required!')
+            .isLength({ min: 5 })
+            .withMessage('Password must be at least 5 characters long.'),
+        body('language').notEmpty().withMessage('Language is required!'),
+    ];
 };
 
 const loginUserValidation = () => {
-  return [
-    body("username").isString().withMessage("O usuário/email é obrigatório!"),
-    body("password")
-      .notEmpty()
-      .withMessage("A senha é obrigatória!")
-      .isString()
-      .withMessage("A senha é obrigatória!"),
-  ];
+    return [
+        body('username').isString().withMessage('User/Email is required!'),
+        body('password')
+            .notEmpty()
+            .withMessage('Password is required!')
+            .isString()
+            .withMessage('Password is required!'),
+    ];
 };
 
 const updateUserValidation = () => {
-  return [
-    body("username")
-      .optional()
-      .isString()
-      .withMessage("O Username é obrigatório!")
-      .isLength({ min: 3 })
-      .withMessage("O Username precisa ter no mínimo 3 caracteres."),
-    body("email")
-      .optional()
-      .isString()
-      .withMessage("O Email é obrigatório!")
-      .isEmail()
-      .withMessage("O Email precisa ser válido!"),
-    body("password")
-      .optional()
-      .isString()
-      .withMessage("A senha é obrigatória!")
-      .isLength({ min: 5 })
-      .withMessage("A senha deve ter ao menos 5 caracteres."),
-    body("language")
-      .optional()
-      .notEmpty()
-      .withMessage("A linguagem é obrigatória!"),
-    body("description")
-      .optional()
-      .isString()
-      .withMessage("")
-      .isLength({ min: 5 })
-      .withMessage("A linguagem é obrigatória!"),
-  ];
+    return [
+        body('username')
+            .optional()
+            .isString()
+            .withMessage('Username is required!')
+            .isLength({ min: 3 })
+            .withMessage('Username must be at least 3 characters long.'),
+        body('email')
+            .optional()
+            .isString()
+            .withMessage('Email is required!')
+            .isEmail()
+            .withMessage('Emais must be valid!'),
+        body('password')
+            .optional()
+            .isString()
+            .withMessage('Password is required!')
+            .isLength({ min: 5 })
+            .withMessage('Password must be at least 5 characters long.'),
+        body('language').optional().notEmpty().withMessage('Language is required!'),
+        body('description')
+            .optional()
+            .isString()
+            .withMessage('')
+            .isLength({ min: 5 })
+            .withMessage('Language is required!'),
+    ];
 };
 
 module.exports = {
-  createUserValidation,
-  loginUserValidation,
-  updateUserValidation,
+    createUserValidation,
+    loginUserValidation,
+    updateUserValidation,
 };
