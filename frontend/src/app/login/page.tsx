5d537e3aa45a836dc43414560fa6d93ea8d00812
Wesley Santos <wesleysantos32892653@outlook.com>
2024-10-08 22:46:25 -0300

    Login Icons

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,18 +6,12 @@ import Link from 'next/link';
 
 //Icons
 import { FaArrowLeft } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import { FaLock } from 'react-icons/fa';
+import { GoEyeClosed } from 'react-icons/go';
+import { GoEye } from 'react-icons/go';
 
 //Shadcn
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@components/ui/form';
-
 import { Input } from '@components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
@@ -25,6 +19,12 @@ import animationData from '../../../public/animation/Animation - Login.json';
 import Lottie from 'lottie-react';
 
 const login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+
     return (
         <main className="bg-background min-h-screen overflow-hidden text-foreground">
             <div className="flex justify-around align-middle text-left mt-16 h-[70vh]">
@@ -43,7 +43,7 @@ const login = () => {
                     <div className="flex flex-col justify-center align-middle items-center">
                         <Lottie
                             animationData={animationData}
-                            loop={false}
+                            loop={true}
                             initialSegment={[20, 300]}
                             style={{ width: '50vw', height: '50vh' }}
                         />
@@ -54,21 +54,34 @@ const login = () => {
                     <p className="text-[var(--secondary-foreground)] text-[1vw] mb-10">
                         It's just few minutes and free!
                     </p>
-                    <form className="bg-[var(--primary)] w-1/2 flex flex-col items-center px-4 rounded-3xl justify-around">
-                        <div className="w-[98%] flex flex-col">
+                    <form className="bg-[var(--primary)] w-1/2 flex flex-col items-center px-4 rounded-3xl justify-around ">
+                        <div className="w-[98%] flex flex-col relative">
                             <label className="mt-4">Email/Username</label>
+                            <MdEmail className="absolute top-14 left-1" size={25} />
                             <input
-                                className="bg-[var(--secondary)] rounded-lg p-2 mt-2"
+                                className="bg-[var(--secondary)] rounded-lg p-2 mt-2 pl-8"
                                 placeholder="Email or username"
                             ></input>
                         </div>
 
-                        <div className="w-[98%] flex flex-col">
+                        <div className="w-[98%] flex flex-col relative">
                             <label className="mt-4">Password</label>
+                            <FaLock
+                                className="absolute top-[3.75em] left-2 opacity-90 mr-10"
+                                size={19}
+                            />{' '}
                             <input
-                                className="bg-[var(--secondary)] rounded-lg p-2 mt-2"
+                                className="bg-[var(--secondary)] rounded-lg p-2 mt-2 pl-8"
+                                type={showPassword ? 'text' : 'password'}
                                 placeholder="Password"
                             ></input>
+                            <span
+                                className="absolute top-14 left-[91%]"
+                                onClick={togglePasswordVisibility}
+                            >
+                                {showPassword ? <GoEye size={23} /> : <GoEyeClosed size={23} />}{' '}
+                                {/* Alterna entre olho aberto e fechado */}
+                            </span>
                         </div>
 
                         <Button
